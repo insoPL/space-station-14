62f8479987f7a6846ba13ac40984469c3206e20f
Łukasz Lindert <lukasz.lindert@protonmail.com>
2025-06-26 22:40:10 +0200

    added my (not working yet) shader

---
+++ Content.Client/BodyOfWaterEffect/BodyOfWaterEffectComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Client.BodyOfWaterEffect;
+
+[RegisterComponent]
+public sealed partial class BodyOfWaterEffectComponent : Component
+{
+
+}
---
+++ Content.Client/BodyOfWaterEffect/BodyOfWaterEffectSystem.cs
@@ -0,0 +1,45 @@
+using Robust.Client.GameObjects;
+using Robust.Client.Graphics;
+using Robust.Shared.Prototypes;
+
+namespace Content.Client.BodyOfWaterEffect;
+
+public sealed partial class BodyOfWaterEffectSystem : EntitySystem
+{
+    [Dependency] private readonly IPrototypeManager _proto = default!;
+
+    private EntityQuery<SpriteComponent> _spriteQuery;
+
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        _spriteQuery = GetEntityQuery<SpriteComponent>();
+
+        SubscribeLocalEvent<BodyOfWaterEffectComponent, ComponentStartup>(OnStartup);
+    }
+    private void OnStartup(Entity<BodyOfWaterEffectComponent> ent, ref ComponentStartup args)
+    {
+        SetShader(ent.Owner, true);
+    }
+
+    private void SetShader(Entity<SpriteComponent?> sprite, bool enabled)
+    {
+        if (!_spriteQuery.Resolve(sprite.Owner, ref sprite.Comp, false))
+            return;
+
+        var shader = _proto.Index<ShaderPrototype>("LakeEffect").Instance();
+
+        if (sprite.Comp.PostShader is not null && sprite.Comp.PostShader != shader)
+            return;
+
+        if (enabled)
+        {
+            sprite.Comp.PostShader = shader;
+        }
+        else
+        {
+            sprite.Comp.PostShader = null;
+        }
+    }
+}
