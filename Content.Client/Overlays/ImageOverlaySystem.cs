60a534bdca74fe592b9821732d1fd2240b64eac5
Łukasz Lindert <lukasz.lindert@protonmail.com>
2025-08-14 22:29:17 +0200

    yeah

--- Content.Client/Overlays/ImageOverlaySystem.cs
+++ Content.Client/Overlays/ImageOverlaySystem.cs
@@ -1,7 +1,14 @@
+using Content.Shared.Hands;
+using Content.Shared.Hands.Components;
 using Content.Shared.Inventory.Events;
 using Content.Shared.Overlays;
+using Content.Shared.Wieldable;
+using Content.Shared.Wieldable.Components;
 using Robust.Client.Graphics;
+using Robust.Client.Timing;
+using Robust.Shared.Containers;
 using Robust.Shared.Prototypes;
+using System.Numerics;
 
 namespace Content.Client.Overlays;
 
@@ -13,6 +20,7 @@ public sealed partial class ImageOverlaySystem : EquipmentHudSystem<ImageOverlay
 {
     [Dependency] private readonly IOverlayManager _overlayMan = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly IClientGameTiming _gameTiming = default!;
 
     public static readonly ProtoId<ShaderPrototype> ImageShader = "ImageMask";
     private ImageOverlay _overlay = new();
@@ -21,7 +29,35 @@ public sealed partial class ImageOverlaySystem : EquipmentHudSystem<ImageOverlay
     {
         base.Initialize();
 
+        SubscribeLocalEvent<ImageOverlayComponent, ItemWieldedEvent>(OnEyeOffsetWielded);
+        SubscribeLocalEvent<ImageOverlayComponent, ItemUnwieldedEvent>(OnEyeOffsetUnwielded);
+        SubscribeLocalEvent<ImageOverlayComponent, GotUnequippedHandEvent>(OnUnequippedHand);
+
         _overlay = new();
+    }
+
+    private void OnUnequippedHand(Entity<ImageOverlayComponent> entity, ref GotUnequippedHandEvent args)
+    {
+        DeactivateInternal();
+    }
+
+    private void OnEyeOffsetUnwielded(Entity<ImageOverlayComponent> entity, ref ItemUnwieldedEvent args)
+    {
+        if (_gameTiming.IsFirstTimePredicted)
+            DeactivateInternal();
+    }
+
+    private void OnEyeOffsetWielded(Entity<ImageOverlayComponent> entity, ref ItemWieldedEvent args)
+    {
+        _overlay.ImageShaders.Clear();
+        var values = new ImageShaderValues
+        {
+            PathToOverlayImage = entity.Comp.PathToOverlayImage,
+            AdditionalColorOverlay = entity.Comp.AdditionalColorOverlay
+        };
+        _overlay.ImageShaders.Add((_prototypeManager.Index(ImageShader).InstanceUnique(), values));
+
+        _overlayMan.AddOverlay(_overlay);
     }
 
     protected override void UpdateInternal(RefreshEquipmentHudEvent<ImageOverlayComponent> component)
