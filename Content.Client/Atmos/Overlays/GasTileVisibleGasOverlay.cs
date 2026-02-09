38f965e033eb36430ecd92c2004606182e66da40
Łukasz Lindert <lukasz.lindert@protonmail.com>
2026-02-10 00:40:12 +0100

    well

--- Content.Client/Atmos/Overlays/GasTileVisibleGasOverlay.cs
+++ Content.Client/Atmos/Overlays/GasTileVisibleGasOverlay.cs
@@ -40,7 +40,7 @@ public sealed class GasTileVisibleGasOverlay : Overlay
 
     private int _gasCount;
 
-    public const int GasOverlayZIndex = (int) Shared.DrawDepth.DrawDepth.Effects; // Under ghosts, above mostly everything else
+    public const int GasOverlayZIndex = (int) Shared.DrawDepth.DrawDepth.Gasses; // Under ghosts, above mostly everything else
 
     public GasTileVisibleGasOverlay(GasTileOverlaySystem system, IEntityManager entManager, IResourceCache resourceCache, IPrototypeManager protoMan, SpriteSystem spriteSys, SharedTransformSystem xformSys)
     {
