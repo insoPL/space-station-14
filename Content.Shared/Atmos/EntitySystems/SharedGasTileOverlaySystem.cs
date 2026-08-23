e9048b338254fbda49624c4fdbebb7d153a23d1d
Łukasz Lindert <lukasz.lindert@protonmail.com>
2026-08-23 20:06:21 +0200

    next part

--- Content.Shared/Atmos/EntitySystems/SharedGasTileOverlaySystem.cs
+++ Content.Shared/Atmos/EntitySystems/SharedGasTileOverlaySystem.cs
@@ -1,7 +1,6 @@
 using Content.Shared.Atmos.Components;
 using Robust.Shared.Configuration;
 using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.Atmos.EntitySystems;
@@ -15,27 +14,7 @@ public abstract partial class SharedGasTileOverlaySystem : EntitySystem
     [Dependency] protected IConfigurationManager ConfMan = default!;
     [Dependency] private SharedAtmosphereSystem _atmosphere = default!;
 
-    /// <summary>
-    ///     array of the ids of all visible gases.
-    /// </summary>
-    public int[] VisibleGasId = default!;
-
-    public override void Initialize()
-    {
-        base.Initialize();
-        SubscribeLocalEvent<GasTileOverlayComponent, ComponentGetState>(OnGetState);
-
-        List<int> visibleGases = new();
-
-        for (var i = 0; i < Atmospherics.TotalNumberOfGases; i++)
-        {
-            var gasPrototype = _atmosphere.GetGas(i);
-            if (gasPrototype.GasOverlaySprite != null)
-                visibleGases.Add(i);
-        }
-        VisibleGasId = visibleGases.ToArray();
-    }
-
+    [SubscribeLocalEvent]
     private void OnGetState(EntityUid uid, GasTileOverlayComponent component, ref ComponentGetState args)
     {
         if (PvsEnabled && !args.ReplayState)
@@ -61,51 +40,6 @@ public abstract partial class SharedGasTileOverlaySystem : EntitySystem
     public static Vector2i GetGasChunkIndices(Vector2i indices)
     {
         return new Vector2i((int)MathF.Floor((float)indices.X / ChunkSize), (int)MathF.Floor((float)indices.Y / ChunkSize));
-    }
-
-    [Serializable, NetSerializable]
-    public readonly struct SharedFireData : IEquatable<SharedFireData>
-    {
-        [ViewVariables] public readonly byte FireState;
-        // TODO change fire color based on ByteTemp
-
-        public SharedFireData(byte fireState)
-        {
-            FireState = fireState;
-        }
-
-        public bool Equals(SharedFireData other)
-        {
-            return FireState == other.FireState;
-        }
-    }
-
-    [Serializable, NetSerializable]
-    public readonly struct SharedVisibleGasData : IEquatable<SharedVisibleGasData>
-    {
-        [ViewVariables] public readonly byte[] Opacity;
-
-        public SharedVisibleGasData(byte[] opacity)
-        {
-            Opacity = opacity;
-        }
-
-        public bool Equals(SharedVisibleGasData other)
-        {
-            if (Opacity?.Length != other.Opacity?.Length)
-                return false;
-
-            if (Opacity != null && other.Opacity != null)
-            {
-                for (var i = 0; i < Opacity.Length; i++)
-                {
-                    if (Opacity[i] != other.Opacity[i])
-                        return false;
-                }
-            }
-
-            return true;
-        }
     }
 
     [Serializable, NetSerializable]
